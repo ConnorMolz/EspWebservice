0c96f7eb8049c41a19ca5c8382ea2fb19bfe8950
ConnorMolz <114417919+ConnorMolz@users.noreply.github.com>
2024-12-24 17:51:22 +0100

    Add Id field

--- sql/init.sql
+++ sql/init.sql
@@ -5,6 +5,7 @@ CREATE DATABASE smart_home;
 CREATE TABLE smart_plants
 (
     id         SERIAL PRIMARY KEY,
+    sensor_id INT   NOT NULL,
     moist      FLOAT NOT NULL,
     temperature FLOAT NOT NULL,
     humidity   FLOAT NOT NULL,
